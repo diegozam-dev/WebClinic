a9bc1bc5320b33c2217234d20c5a6281f35033de
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2024-12-27 16:20:36 -0500

    add home section to the landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { BtnPrimaryComponent } from '../components/buttons/btn-primary/btn-primary.component';
+import { LandingHeaderComponent } from './components';
+import { Router } from '@angular/router';
+import { ButtonSize } from '../enums/buttonSize.enum';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [BtnPrimaryComponent],
+  imports: [BtnPrimaryComponent, LandingHeaderComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
 })
-export class LandingPageComponent {}
+export class LandingPageComponent {
+  public buttonSize = ButtonSize;
+
+  constructor(private router: Router) {}
+
+  public goToSignup(): void {
+    this.router.navigate(['/signup']);
+  }
+}
