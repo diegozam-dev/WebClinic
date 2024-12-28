c43801c3143d4016e9be82150274982bdc96e138
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2024-12-28 16:07:47 -0500

    add responsive styles

--- src/app/landing-page/components/landing-header/landing-header.component.ts
+++ src/app/landing-page/components/landing-header/landing-header.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { BtnPrimaryComponent } from '../../../components';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-header',
   standalone: true,
-  imports: [BtnPrimaryComponent],
+  imports: [BtnPrimaryComponent, CommonModule],
   templateUrl: './landing-header.component.html',
   styleUrl: './landing-header.component.css',
 })
 export class LandingHeaderComponent {
+  isMenuOpen = signal(false);
+
   constructor(private router: Router) {}
 
   public goToLogin(): any {
     // this.router.navigate(['/login']);
     console.log('Holaaaa');
+  }
+
+  public changeMenuState(): void {
+    this.isMenuOpen.set(!this.isMenuOpen());
+  }
+
+  public onResize(windowSize: number) {
+    if (windowSize > 768) this.isMenuOpen.set(false);
   }
 }
