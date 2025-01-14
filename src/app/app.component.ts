c4fcfad9d8953cd98bf64e7324f4fab5e4a709e9
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-01-13 20:14:13 -0500

    add login and register routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AuthModule } from './auth/auth.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AuthModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export default [
   {
-    path: '',
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component'),
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.component'),
   },
 ] as Routes;
